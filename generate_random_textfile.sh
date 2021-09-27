6558e80685bb24efd665da1aed18afa53b11e899
jf18j492 <jonas.frey@csh.unibe.ch>
2021-09-27 10:55:02 +0200

    learning BOM byte order mark

--- generate_random_textfile.sh
+++ generate_random_textfile.sh
@@ -17,3 +17,25 @@ done
 echo $random_content
 
 echo -n -e $random_content > generate_ranom_textfile_textfile.txt
+
+
+filename_256bytes="256bytes.txt"
+echo "generate $filename_256bytes"
+
+characters=256
+#first three bytes are BOM => byte order mark 
+BOM="\xef\xbb\xbf"
+content=""
+# for i in {1..$characters}
+for ((c=0; c<=$characters; c++))
+do
+    num_in_hex="$(printf '%x\n' $c)"
+    # echo "Welcome $random_number times"
+    # echo "$number_base_16_exp_1"
+    content+="\x$num_in_hex"
+done
+echo $content
+
+echo -n -e $BOM$content > "256_bytes_with_bom_ef_bb_bf.txt"
+
+echo -n -e $content > "256_bytes_no_bom.txt"
